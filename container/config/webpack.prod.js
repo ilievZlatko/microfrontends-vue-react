384571c73896837aeb8ede76ccb6656a80758392
zlatkoiliev <83271119+zlatkoiliev@users.noreply.github.com>
2021-10-15 09:54:31 +0300

    try to fix build

--- container/config/webpack.prod.js
+++ container/config/webpack.prod.js
@@ -4,7 +4,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
+const domain = process.env.PRODUCTION_DOMAIN || 'https://d20ho5b25w4h03.cloudfront.net';
 
 const prodConfig = {
 	mode: 'production',
